e39bfbeb70cf239ba7bcbe96b5c231d6d3140cc1
bruh <quangtrung02hn16@gmail.com>
2023-11-03 07:28:59 +0700

    More docs

--- theming/src/main/java/com/sixtyninefourtwenty/theming/ActivityTheming.kt
+++ theming/src/main/java/com/sixtyninefourtwenty/theming/ActivityTheming.kt
@@ -16,12 +16,16 @@ import com.sixtyninefourtwenty.theming.preferences.ThemingPreferencesSupplier
 import com.sixtyninefourtwenty.theming.preferences.addThemingPreferences
 
 /**
+ * Apply theming to an [Activity] according to settings.
+ * @param material2ThemeStyleRes Resource ID of your theme that extends [R.style.AppTheme_Material2].
+ * @param material3CustomColorsThemeStyleRes Resource ID of your theme that extends
+ * [R.style.AppTheme_Material3_CustomColors].
+ * @param material3DynamicColorsThemeStyleRes Resource ID of your theme that extends
+ * [R.style.AppTheme_Material3_DynamicColors].
  * @param preferencesSupplier Preference storage to use if available. If
  * you're using [PreferenceGroup.addThemingPreferences] on a [PreferenceFragmentCompat],
  * make sure that the supplier is linked to whatever [SharedPreferences] or [PreferenceDataStore]
- * the fragment is using.
- *
- * If null, the default storage used by [ThemingPreferenceFragment] will be used.
+ * the fragment is using. If null, the default storage used by [ThemingPreferenceFragment] will be used.
  */
 @JvmOverloads
 fun Activity.applyTheming(
