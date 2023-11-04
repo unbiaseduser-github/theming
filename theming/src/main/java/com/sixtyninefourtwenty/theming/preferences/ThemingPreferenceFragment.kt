35c3fb06eea815e79a7543bc32340a000e9614f3
bruh <quangtrung02hn16@gmail.com>
2023-11-04 11:34:17 +0700

    ThemingPreferenceFragment is just regular PreferenceFragmentCompat

--- theming/src/main/java/com/sixtyninefourtwenty/theming/preferences/ThemingPreferenceFragment.kt
+++ theming/src/main/java/com/sixtyninefourtwenty/theming/preferences/ThemingPreferenceFragment.kt
@@ -1,21 +1,30 @@
 package com.sixtyninefourtwenty.theming.preferences
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceGroup
-import com.sixtyninefourtwenty.custompreferences.PreferenceFragmentCompatAccommodateCustomDialogPreferences
+import com.sixtyninefourtwenty.custompreferences.AbstractCustomDialogPreference
 
 /**
  * Pre-made [PreferenceFragmentCompat] that contains theming preferences added by
  * [PreferenceGroup.addThemingPreferences]. This fragment uses an internal preferences storage.
  */
-class ThemingPreferenceFragment : PreferenceFragmentCompatAccommodateCustomDialogPreferences() {
+class ThemingPreferenceFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         val prefs = ThemingPreferences(requireContext())
         preferenceManager.preferenceDataStore = prefs
         preferenceScreen = preferenceManager.createPreferenceScreen(requireContext()).apply {
             addThemingPreferences(requireActivity(), prefs.lightDarkMode, prefs.md3, prefs.useM3CustomColorThemeOnAndroid12)
+        }
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is AbstractCustomDialogPreference) {
+            preference.displayDialog(this)
+        } else {
+            super.onDisplayPreferenceDialog(preference)
         }
     }
 
