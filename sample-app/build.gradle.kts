d13219466624f4850b392b13b9d3921ea5daa256
bruh <quangtrung02hn16@gmail.com>
2023-11-22 11:03:31 +0700

    Re-enable sample app

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
 }
 
 dependencies {
-
     implementation(project(":theming"))
+    implementation("com.github.unbiaseduser.library-integrations:theming-preference-integration:1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
