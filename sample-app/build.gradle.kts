cd0ff098aa996f7c07ff90d0af07221e5ab55b93
bruh <quangtrung02hn16@gmail.com>
2024-06-28 21:00:23 +0700

    Bump stuff

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
         viewBinding = true
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -40,14 +41,15 @@ android {
 
 dependencies {
     implementation(project(":theming"))
-    implementation("com.github.unbiaseduser.library-integrations:theming-preference-integration:1.0.2")
+    implementation("com.github.unbiaseduser-github.library-integrations:theming-preference-integration:1.1.0")
     implementation("io.github.amrdeveloper:codeview:1.3.9")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
