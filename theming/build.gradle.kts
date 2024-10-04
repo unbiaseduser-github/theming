313d6580378e61e9a5600f4fa1bd4a47c7a6166c
bruh <quangtrung02hn16@gmail.com>
2024-10-04 13:13:32 +0700

    Bump version

--- theming/build.gradle.kts
+++ theming/build.gradle.kts
@@ -48,7 +48,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.sixtyninefourtwenty"
                 artifactId = "theming"
-                version = "2.3.2"
+                version = "2.3.3"
 
                 from(components["release"])
 
